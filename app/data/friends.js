c6c4ed085effbb5939c5f8cfc5bc37f25b79ff41
dschneibolk <dschneibolk@gmail.com>
2019-04-16 16:18:35 -0700

    fixed app

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+let friendsArray = [
+  {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ],
+
+    "name": "Adrienne",
+    "photo": " ",
+    "scores": [
+      4,
+      2,
+      1,
+      5,
+      2,
+      3,
+      3,
+      3,
+      1,
+      4
+    ]
+  }
+]
+
+module.exports = friendsArray;
